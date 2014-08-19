0eb345bea896df1f92b8920f2d951a4a86d354a3
Chad Campbell <chad.campbell@gmail.com>
2014-08-19 11:05:34 -0400

    Updated documentation for $cordovaNetwork.

--- src/network.js
+++ src/network.js
@@ -15,17 +15,40 @@
  * limitations under the License.
 */
 
-/*
- * A service for testing networked fetures 
+/**
+ * @ngdoc service
+ * @name ngCordovaMocks.cordovaNetwork
+ *
+ * @description
+ * A service for testing networked fetures
  * in an app build with ngCordova.
- */ 
+ */  
 ngCordovaMocks.factory('$cordovaNetwork', function () {
 	var connectionType = 'WiFi connection';
 	var isConnected = true;
 
 	return {
-		// Properties intended to mock test scenarios
+        /**
+		 * @ngdoc property
+		 * @name connectionType
+		 * @propertyOf ngCordovaMocks.cordovaNetwork
+		 *
+		 * @description
+		 * They type of connection. Values should match those found at
+		 * https://github.com/apache/cordova-plugin-network-information/blob/master/doc/index.md 
+		 * This property should only be used in automated tests.
+		**/	
 		connectionType: connectionType,
+
+        /**
+		 * @ngdoc property
+		 * @name isConnected
+		 * @propertyOf ngCordovaMocks.cordovaNetwork
+		 *
+		 * @description
+		 * A flag that signals whether the app is connected to a network. 
+		 * This property should only be used in automated tests.
+		**/			
 		isConnected: isConnected,
 
 		getNetwork: function () {
