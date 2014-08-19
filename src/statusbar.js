b6a9e20352b631b84c7db7d8bef5605882eb932c
Chad Campbell <chad.campbell@gmail.com>
2014-08-19 08:10:56 -0400

    Added $cordovaStatusbar to ngCordovaMocks.

---
+++ src/statusbar.js
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2014 Ecofic LLC. All rights reserved.
+ * http://www.ecofic.com
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/**
+ * @ngdoc service
+ * @name cordovaStatusBar
+ *
+ * @description
+ * A service for testing the status bar
+ * in an app build with ngCordova.
+ *
+ * @example
+ */ 
+ngCordovaMocks.factory('$cordovaStatusbar', function() {
+	var isStatusBarVisible = true;
+	var canOverlayWebView = true;
+
+	return {
+		/**
+		 * @ngdoc property
+		 * @name isStatusBarVisible
+		 * @propertyOf ngCordovaMocks.isStatusBarVisible
+		 *
+		 * @description
+		 * A flag that signals whether the status bar is visible or not.
+		 * It is intended for testing purposes only.
+		**/		
+		isStatusBarVisible: isStatusBarVisible,
+
+		/**
+		 * @ngdoc property
+		 * @name canOverlayWebView
+		 * @propertyOf ngCordovaMocks.cordovaStatusbar
+		 *
+		 * @description
+		 * A flag that signals whether the status bar can overlay the web view.
+		 * It is intended for testing purposes only.
+		**/			
+		canOverlayWebView: canOverlayWebView,
+
+		overlaysWebView: function(bool) {
+			this.canOverlayWebView = bool;
+		},
+
+		style: function(style) {
+			// TODO: Review
+			return style;
+		},
+
+		styleHex: function(colorHex) {
+			// TODO: review
+			return colorHex;
+		},
+
+		styleColor: function(color) {
+			// TODO: review
+			return color;
+		},
+
+		hide: function() {
+			this.isStatusBarVisible = false;
+		},
+
+		show: function() {
+			this.isStatusBarVisible = true;
+		},
+
+		isVisible: function () {
+			return this.isStatusBarVisible;
+		}
+	};
+});
