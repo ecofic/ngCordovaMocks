7d920845b95861e1c8565e1d4b7b09b6841dcbcd
Mackenzie Browne <mackbrowne@gmail.com>
2014-09-23 13:14:22 -0400

    add keyboard mock

--- dist/ngCordovaMocks.js
+++ dist/ngCordovaMocks.js
@@ -1527,6 +1527,71 @@ ngCordovaMocks.factory('$cordovaGlobalization', ['$q', function($q) {
 }]);
 
 
+ngCordovaMocks.factory('$cordovaKeyboard', function() {
+	var isStatusBarVisible = true;
+	var canOverlayWebView = true;
+
+	return {
+		/**
+		 * @ngdoc property
+		 * @name isStatusBarVisible
+		 * @propertyOf ngCordovaMocks.cordovaStatusbar
+		 *
+		 * @description
+		 * A flag that signals whether the status bar is visible or not.
+		 * This property should only be used in automated tests.
+		**/		
+		isStatusBarVisible: isStatusBarVisible,
+
+		/**
+		 * @ngdoc property
+		 * @name canOverlayWebView
+		 * @propertyOf ngCordovaMocks.cordovaStatusbar
+		 *
+		 * @description
+		 * A flag that signals whether the status bar can overlay the web view.
+		 * This property should only be used in automated tests.
+		**/			
+		canOverlayWebView: canOverlayWebView,
+
+		overlaysWebView: function(bool) {
+			this.canOverlayWebView = bool;
+		},
+
+		style: function(style) {
+			// TODO: Review
+			return style;
+		},
+
+		hideAccessoryBar: function(status) {
+			return status;
+		},
+
+		styleHex: function(colorHex) {
+			// TODO: review
+			return colorHex;
+		},
+
+		styleColor: function(color) {
+			// TODO: review
+			return color;
+		},
+
+		hide: function() {
+			this.isStatusBarVisible = false;
+		},
+
+		show: function() {
+			this.isStatusBarVisible = true;
+		},
+
+		isVisible: function () {
+			return this.isStatusBarVisible;
+		}
+	};
+});
+
+
 /**
  * @ngdoc service
  * @name ngCordovaMocks.cordovaNetwork
